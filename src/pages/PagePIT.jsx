f03836500e12cf2f395ddd0bb459cf03db288e55
Jefferson Uchôa Ponte <jefponte@gmail.com>
2021-09-30 00:11:07 -0300

    Using axios

--- src/pages/PagePIT.jsx
+++ src/pages/PagePIT.jsx
@@ -3,35 +3,36 @@ import { Container, Typography } from "@mui/material";
 import React, { useState } from "react";
 import FormLogin from "../components/FormLogin";
 import { DataContext } from "../services/DataContext";
+import api from "../services/api";
 
 function PagePIT() {
   const [data, setData] = useState(undefined);
 
-  function fazerLogin(user) {
-    console.log(user);
+  async function handleLogin(user) {
+    await api
+      .post("/authenticate", user)
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((erro) => console.log(erro));
   }
   return (
     <DataContext.Provider value={data}>
       <Container maxWidth="sm">
-      <br/>
-      <Typography
-            component="h1"
-            variant="h4"
-            align="center"
-            color="textPrimary"
-            gutterBottom
-          >
-            Plano Individual de Trabalho (PIT)
-          </Typography>
-          <Typography
-            variant="h5"
-            align="center"
-            color="textSecondary"
-            paragraph
-          >
-            Utilize o formulário abaixo para gerar o PIT.
-          </Typography>
-        <FormLogin onSubmitForm={fazerLogin} />
+        <br />
+        <Typography
+          component="h1"
+          variant="h4"
+          align="center"
+          color="textPrimary"
+          gutterBottom
+        >
+          Plano Individual de Trabalho (PIT)
+        </Typography>
+        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+          Utilize o formulário abaixo para gerar o PIT.
+        </Typography>
+        <FormLogin onSubmitForm={handleLogin} />
       </Container>
     </DataContext.Provider>
   );
